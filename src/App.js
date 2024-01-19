3fc5899732c06905845d499325de83a80d49f7ce
Eetu <99178022+E3TU@users.noreply.github.com>
2024-01-19 12:08:23 +0200

    Ui work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <form className="register-form">
+          <input className='form-field' placeholder='First Name' name='firstName'></input>
+          <input className='form-field' placeholder='Last Name' name="lastName"></input>
+          <input className='form-field' placeholder='Email' name='email'></input>
+        </form>
+        <button id="register-btn">Register</button>
+      </div>
     </div>
   );
 }
